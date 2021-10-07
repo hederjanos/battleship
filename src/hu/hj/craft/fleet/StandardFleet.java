b7871b97fe5b4f16cc37fb9430cc5d1289509088
hederjanos <hederjanos@gmail.com>
2021-10-07 13:57:00 +0200

    Create StandardFleet class.

---
+++ src/hu/hj/craft/fleet/StandardFleet.java
@@ -0,0 +1,31 @@
+package hu.hj.craft.fleet;
+
+import hu.hj.craft.ships.*;
+import hu.hj.craft.subs.Submarine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StandardFleet {
+
+    private static final Map<Class<?>, Integer> FLEET = new HashMap<>();
+
+    static {
+        try {
+            FLEET.putIfAbsent(Class.forName(Battleship.class.getCanonicalName()), 1);
+            FLEET.putIfAbsent(Class.forName(Carrier.class.getCanonicalName()), 1);
+            FLEET.putIfAbsent(Class.forName(Cruiser.class.getCanonicalName()), 1);
+            FLEET.putIfAbsent(Class.forName(Destroyer.class.getCanonicalName()), 1);
+            FLEET.putIfAbsent(Class.forName(Submarine.class.getCanonicalName()), 1);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private StandardFleet() {
+    }
+
+    public static Map<Class<?>, Integer> getFleet() {
+        return FLEET;
+    }
+}
